82d04ef5a2596e2fcdc7ac633b2f0167e79f47cd
Chowdary-Hari <chandrahari321@outlook.com>
2024-09-23 09:13:07 +0530

    automated commit #92

--- terraform/03-bastion/userdata.sh
+++ terraform/03-bastion/userdata.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-# Install Ansible using pip and log the output to /opt/userdata.log
-pip3.11 install ansible 2>&1 | tee -a /opt/userdata.log
-
-# Run ansible-pull to pull the playbook from the specified Git repository
-# Log the output to /opt/userdata.log, and ensure errors are captured as well
-ansible-pull -i localhost, -U https://github.com/wavedevops/expense-ansible main.yaml \
-  -e component="${component}" 2>&1 | tee -a /opt/userdata.log
+dnf install ansible -y
+cd /tmp
+git clone https://github.com/wavedevops/expense-ansible.git
+cd wavedevops/expense-ansible
+#ansible-playbook main.yml -e component=backend -e login_password=ExpenseApp1
+ansible-playbook main.yml -e component=frontend
